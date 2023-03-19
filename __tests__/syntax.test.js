1749bae4ad991512e866a49d1239dd586f37fce6
Daniel Abel <daniel@zkreations.com>
2023-03-19 10:22:51 -0500

    test: perform the tests on all XML files

--- __tests__/syntax.test.js
+++ __tests__/syntax.test.js
@@ -3,52 +3,57 @@
 /* eslint-disable no-undef */
 const fs = require('fs')
 const chai = require('chai')
+const glob = require('glob')
 const cheerio = require('cheerio')
 const { expect } = chai
 
 describe('Blogger Syntax', () => {
-  const template = fs.readFileSync('./dist/output.xml', 'utf-8')
-  const $ = cheerio.load(template, { xmlMode: true })
-  const widgets = $('b\\:widget')
+  const files = glob.sync('./dist/*.xml')
 
-  it('all <b:widget> tags should have required attributes and correct format', function () {
-    const validTypes = [
-      'AdSense', 'Blog', 'BlogArchive', 'BlogList', 'BlogSearch',
-      'ContactForm', 'FeaturedPost', 'Feed', 'Header', 'HTML',
-      'Image', 'Label', 'LinkList', 'PageList', 'PopularPosts',
-      'Profile', 'Stats', 'Subscribe', 'Text', 'TextList',
-      'Translate', 'Wikipedia'
-    ]
+  files.forEach((file) => {
+    const template = fs.readFileSync(file, 'utf-8')
+    const $ = cheerio.load(template, { xmlMode: true })
+    const widgets = $('b\\:widget')
 
-    widgets.each(function () {
-      const id = $(this).attr('id')
-      const type = $(this).attr('type')
+    it('all <b:widget> tags should have required attributes and correct format', function () {
+      const validTypes = [
+        'AdSense', 'Blog', 'BlogArchive', 'BlogList', 'BlogSearch',
+        'ContactForm', 'FeaturedPost', 'Feed', 'Header', 'HTML',
+        'Image', 'Label', 'LinkList', 'PageList', 'PopularPosts',
+        'Profile', 'Stats', 'Subscribe', 'Text', 'TextList',
+        'Translate', 'Wikipedia'
+      ]
 
-      expect(id).to.exist
-      expect(type).to.exist
-      expect(validTypes).to.include(type)
+      widgets.each(function () {
+        const id = $(this).attr('id')
+        const type = $(this).attr('type')
 
-      const idRegex = /^[A-Za-z]+[0-9]{1,3}$/
-      expect(id).to.match(idRegex)
-      expect(id).to.match(new RegExp(`${type}\\d+$`))
+        expect(id).to.exist
+        expect(type).to.exist
+        expect(validTypes).to.include(type)
+
+        const idRegex = /^[A-Za-z]+[0-9]{1,3}$/
+        expect(id).to.match(idRegex)
+        expect(id).to.match(new RegExp(`${type}\\d+$`))
+      })
     })
-  })
 
-  it('all <b:widget> tags should have a unique `id` attribute', function () {
-    const idList = []
+    it('all <b:widget> tags should have a unique `id` attribute', function () {
+      const idList = []
 
-    widgets.each(function () {
-      const id = $(this).attr('id')
-      expect(id).to.exist
-      expect(idList).not.to.contain(id)
-      idList.push(id)
+      widgets.each(function () {
+        const id = $(this).attr('id')
+        expect(id).to.exist
+        expect(idList).not.to.contain(id)
+        idList.push(id)
+      })
     })
-  })
 
-  it('all <b:widget> tags should be direct children of <b:section>', function () {
-    widgets.each(function () {
-      const parent = $(this).parent().get(0).tagName
-      expect(parent).to.equal('b:section')
+    it('all <b:widget> tags should be direct children of <b:section>', function () {
+      widgets.each(function () {
+        const parent = $(this).parent().get(0).tagName
+        expect(parent).to.equal('b:section')
+      })
     })
   })
 })
--- __tests__/template.test.js
+++ __tests__/template.test.js
@@ -2,32 +2,37 @@
 /* eslint-disable no-undef */
 const fs = require('fs')
 const chai = require('chai')
+const glob = require('glob')
 const cheerio = require('cheerio')
 const { expect } = chai
 
 describe('Blogger template', () => {
-  const template = fs.readFileSync('./dist/output.xml', 'utf-8')
-  const $ = cheerio.load(template, { xmlMode: true })
+  const files = glob.sync('./dist/*.xml')
 
-  it('should start with <?xml version="1.0" encoding="utf-8"?>', function () {
-    expect(template.startsWith('<?xml version="1.0" encoding="utf-8"?>')).to.be.true
-  })
+  files.forEach((file) => {
+    const template = fs.readFileSync(file, 'utf-8')
+    const $ = cheerio.load(template, { xmlMode: true })
 
-  it('should contain a <b:skin> tag', function () {
-    expect(template).to.include('<b:skin>')
-  })
+    it('should start with <?xml version="1.0" encoding="utf-8"?>', function () {
+      expect(template.startsWith('<?xml version="1.0" encoding="utf-8"?>')).to.be.true
+    })
 
-  it('should contain at least one <b:section> tag with a unique id attribute', function () {
-    const sections = $('b\\:section')
-    const idList = []
+    it('should contain a <b:skin> tag', function () {
+      expect(template).to.include('<b:skin>')
+    })
 
-    expect(sections.length).to.be.greaterThan(0)
+    it('should contain at least one <b:section> tag with a unique id attribute', function () {
+      const sections = $('b\\:section')
+      const idList = []
 
-    sections.each(function () {
-      const id = $(this).attr('id')
-      expect(id).to.exist
-      expect(idList).not.to.contain(id)
-      idList.push(id)
+      expect(sections.length).to.be.greaterThan(0)
+
+      sections.each(function () {
+        const id = $(this).attr('id')
+        expect(id).to.exist
+        expect(idList).not.to.contain(id)
+        idList.push(id)
+      })
     })
   })
 })
